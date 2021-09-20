4422a5712b6b1ea66d760d93bc791e257b5d59b7
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2021-09-20 12:24:48 +0200

    Add formula for previous, working u-boot-tools release

---
+++ Formula/u-boot-tools@2021.01.rb
@@ -0,0 +1,33 @@
+class UBootToolsAT202101 < Formula
+  desc "Universal boot loader"
+  homepage "https://www.denx.de/wiki/U-Boot/"
+  url "https://ftp.denx.de/pub/u-boot/u-boot-2021.01.tar.bz2"
+  sha256 "b407e1510a74e863b8b5cb42a24625344f0e0c2fc7582d8c866bd899367d0454"
+  license all_of: ["GPL-2.0-only", "GPL-2.0-or-later", "BSD-3-Clause"]
+
+  livecheck do
+    url "https://ftp.denx.de/pub/u-boot/"
+    regex(/href=.*?u-boot[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "openssl@1.1"
+
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex" => :build
+
+  def install
+    # Replace keyword not present in make 3.81
+    inreplace "Makefile", "undefine MK_ARCH", "unexport MK_ARCH"
+
+    system "make", "sandbox_defconfig"
+    system "make", "tools", "NO_SDL=1"
+    bin.install "tools/mkimage"
+    bin.install "tools/dumpimage"
+    man1.install "doc/mkimage.1"
+  end
+
+  test do
+    system bin/"mkimage", "-V"
+    system bin/"dumpimage", "-V"
+  end
+end
