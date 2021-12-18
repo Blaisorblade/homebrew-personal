46d22e5ccf543aa51ec91d36cefc020e8045f180
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2021-12-18 11:38:07 +0100

    Update syntax

--- Formula/llvm@10.0.0_3.rb
+++ Formula/llvm@10.0.0_3.rb
@@ -61,8 +61,7 @@ class LlvmAT10003 < Formula
 
   bottle do
     root_url "https://homebrew.bintray.com/bottles"
-    sha256 "ea9b9f579df49499d9ab0084e10edecc5350298d6c5db399a1dabc8694dab7db" => :catalina
-    cellar :any
+    sha256 cellar: :any, catalina: "ea9b9f579df49499d9ab0084e10edecc5350298d6c5db399a1dabc8694dab7db"
     # sha256 "14f59a25e73e3a00fd36632f2106b41eda1b54aa1039b4b979bd957a8c041bf4" => :mojave
     # sha256 "6e09ca233790a58edae55bba453fd50179369b5514acb5f8b86156401227a75e" => :high_sierra
   end
